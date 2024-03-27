abf3a565b21a88e3efae551c10fedeb78a9d7798
jihwan160 <dark160@naver.com>
2024-03-27 15:58:09 +0900

    gnb 완성

--- src/Pages/Home/Gnb.js
+++ src/Pages/Home/Gnb.js
@@ -1,39 +1,62 @@
 import React from 'react'
-// import './Gnb.css'
 import logo from './../../img/tving-logo.svg'
 import icon from './../../img/tving_icon.webp'
-
+import styled from 'styled-components'
 
 const Gnb = () => {
 
-const GnbArea = () => {
-    return(
-        // <div classNameName='gnb'>
-        //     <div className="header">
-        //         <div className="header_cover">
-        //         <div className="gnb">
-        //             <h1 className="logo">
-        //                 <img src={logo} alt="logo" />
-        //             </h1>
-        //             <ul className="cf">
-        //             <li>시리즈</li>
-        //             <li>영화</li>
-        //             <li>라이브</li>
-        //             <li>파라마운트+</li>
-        //             </ul>
-        //         </div>
-        //         <div className="search_info">
-        //             <button><i className="fa-solid fa-magnifying-glass"></i></button>
-        //             <div className="info">
-        //                 <img src={icon} alt="tving_icon" />
-        //             </div>
-        //         </div>
-        //         </div>
-        //     </div>
-        // </div>
 
+
+const GnbArea = () => {
+
+const GnbLeft = styled.div`
+  height: 100%;
+  display : flex;
+  align-items: center;
+`;
+
+const GnbRight = styled(GnbLeft)`
+  display : flex;
+  align-items: center;
+`;
+
+const Ul = styled.ul`
+  display: flex;
+  margin-left: 40px;
+`;
+
+const Li = styled.li`
+  cursor: pointer;
+  margin-right: 40px;
+  color: #d9d9d9;
+`
+
+const GnbCover = styled.div`
+  padding: 0 40px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  height: 75px;
+  background: #000;
+`
+
+    return(
         <div>
-            
+            <GnbCover>
+              <GnbLeft>
+                <img src={logo} alt='logo' style={{height:"100%"}} />
+                <Ul>
+                  <Li>시리즈</Li>
+                  <Li>영화</Li>
+                  <Li>라이브</Li>
+                  <Li>파라마운트+</Li>
+                </Ul>
+              </GnbLeft>
+              <GnbRight>
+                <div><i className="fa-solid fa-magnifying-glass" style={{cursor:"pointer",color:"#d9d9d9"}}></i></div>
+                <div style={{marginLeft:"40px"}}><img src={icon} alt='logo' style={{width:"30px", height:"30px", cursor:"pointer"}} /></div>
+              </GnbRight>
+            </GnbCover>
         </div>
     )
 }
