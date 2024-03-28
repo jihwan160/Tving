64fd320e852779b545450e3eff94dfd99eb93ea2
jihwan160 <dark160@naver.com>
2024-03-28 16:33:55 +0900

    swiper 추가

--- src/Pages/Home/Gnb.js
+++ src/Pages/Home/Gnb.js
@@ -1,13 +1,17 @@
 import React from 'react'
-import logo from './../../img/tving-logo.svg'
-import icon from './../../img/tving_icon.webp'
 import styled from 'styled-components'
 
-const Gnb = () => {
+import logo from './../../img/tving-logo.svg'
+import icon from './../../img/tving_icon.webp'
+
+import { Navigation, Pagination } from 'swiper'
+import {Swiper, SwiperSlide} from 'swiper/react'
+
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
 
 
-
-const GnbArea = () => {
 
 const GnbLeft = styled.div`
   height: 100%;
@@ -40,6 +44,21 @@ const GnbCover = styled.div`
   background: #000;
 `
 
+const SwiperBox = styled.div`
+  width: 95%;
+  height: 95%;
+  margin: 0 auto;
+  margin-top: 20px;
+`;
+
+
+
+
+const Gnb = () => {
+
+
+const GnbArea = () => {
+
     return(
         <div>
             <GnbCover>
@@ -61,11 +80,37 @@ const GnbCover = styled.div`
     )
 }
 
+const SwiperImg = () => {
 
+  const imgList = [
+      './../../img/swi_1.png',
+      './../../img/swi_1.png',
+    ]
+
+  return(
+    <>
+      <Swiper
+        modules={[Navigation, Pagination]}
+        navigation
+        pagination={{ clickable: true }}
+        style={{height:"700px"}}
+      >
+        {imgList.map((img,key) => (
+          <SwiperSlide key={key}>
+            <SwiperBox>
+              <img src={img} alt={`swi_${key}`} style={{width:"100%",height:"100%",borderRadius:"10px"}}/>
+            </SwiperBox>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </>
+  )
+}
 
   return (
     <>
         <GnbArea />
+        <SwiperImg />
     </>
   )
 }
